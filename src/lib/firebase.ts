a0adf048543cc3336c9075b29f0334128126fdf6
Platypus96 <adarshkr1380@gmail.com>
2025-07-27 13:37:54 +0530

    Update header and other components

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,6 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "nexusfind-7l43n",
-  appId: "1:470473911552:web:90bae86a84680e809d17e5",
-  storageBucket: "nexusfind-7l43n.firebasestorage.app",
-  apiKey: "AIzaSyBixkb8aWV6C-RKqJs0VKwiD6TZenmhbDU",
-  authDomain: "nexusfind-7l43n.firebaseapp.com",
-  messagingSenderId: "470473911552"
 };
 
 // Initialize Firebase
